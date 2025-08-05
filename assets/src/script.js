203a5ffaf891ee81539a8d3465af9ff33aa2e747
Artur Roma Furtado <arturromafurtado@gmail.com>
2025-08-05 18:00:44 -0300

    Testes de chamada com console.log

--- assets/src/script.js
+++ assets/src/script.js
@@ -1,6 +1,16 @@
 console.log('Script carregado!')
 
 function calcular() {
-      console.log("Função calcular chamada");
+    console.log("Função calcular chamada");
+    const input1 = document.getElementById('input01');
+    const input2 = document.getElementById('input02');
+    const valor1 = Number(input1.value);
+    const valor2 = Number(input2.value);
+    console.log(valor1);
+    console.log(valor2);
+
+    const operacao = document.getElementById('operacao');
+    const tipoOperacao = operacao.value;
+    console.log(tipoOperacao); 
 }
 
