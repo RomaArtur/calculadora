376f142a011cea630a6cec1b982df48bdc96c3a5
Artur Roma Furtado <arturromafurtado@gmail.com>
2025-08-05 18:10:02 -0300

    Switch simples para calcular resultado de operações

--- assets/src/script.js
+++ assets/src/script.js
@@ -2,15 +2,39 @@ console.log('Script carregado!')
 
 function calcular() {
     console.log("Função calcular chamada");
-    const input1 = document.getElementById('input01');
-    const input2 = document.getElementById('input02');
-    const valor1 = Number(input1.value);
-    const valor2 = Number(input2.value);
+    let input1 = document.getElementById('input01');
+    let input2 = document.getElementById('input02');
+    let valor1 = Number(input1.value);
+    let valor2 = Number(input2.value);
     console.log(valor1);
     console.log(valor2);
 
-    const operacao = document.getElementById('operacao');
-    const tipoOperacao = operacao.value;
+    let operacao = document.getElementById('operacao');
+    let tipoOperacao = operacao.value;
     console.log(tipoOperacao); 
+
+    let resultado;
+
+    switch (tipoOperacao) {
+        case "+":
+            resultado = valor1 + valor2;
+            break;
+        case "-":
+            resultado = valor1 - valor2;
+            break;
+        case "*":
+            resultado = valor1 * valor2;
+            break;
+        case "/":
+            resultado = valor1 / valor2;
+            break;
+        default:
+            resultado = alert('Operação inválida');
+    }
+
+    console.log(resultado);
+
 }
+
+
 
