ea607f30bd19ef5461923d990a1749a17612ccbb
Artur Roma Furtado <arturromafurtado@gmail.com>
2025-08-05 22:25:08 -0300

    Remoção console.log, css simples + alguns ajustes

--- src/script.js
+++ src/script.js
@@ -1,18 +1,14 @@
-console.log("Script carregado!");
+
 
 function calcular() {
-  console.log("Função calcular chamada");
   let elementoInput01 = document.getElementById("input01");
   let elementoInput02 = document.getElementById("input02");
+  let elementoOperacao = document.getElementById("operacao");
   let elementoResultado = document.getElementById("resultado");
+
   let valor1 = Number(elementoInput01.value);
   let valor2 = Number(elementoInput02.value);
-  console.log(valor1);
-  console.log(valor2);
-
-  let elementoOperacao = document.getElementById("operacao");
-  let tipoOperacao = operacao.value;
-  console.log(tipoOperacao);
+  let tipoOperacao = elementoOperacao.value;
 
   let resultado;
 
@@ -34,17 +30,17 @@ function calcular() {
         resultado = "";
       }
   }
+
   elementoResultado.innerText = `Resultado: ${resultado}`;
-  console.log(resultado);
 }
 
 function limpar() {
-    let elementoInput01 = document.getElementById("input01");
-    let elementoInput02 = document.getElementById("input02");
-    let elementoOperacao = document.getElementById('operacao');
-    let elementoResultado = document.getElementById("resultado");
-    input1.value = '';
-    input2.value = '';
-    operacao.selectedIndex = 0
-    resultado.innerText = 'Resultado: ';
+  let elementoInput01 = document.getElementById("input01");
+  let elementoInput02 = document.getElementById("input02");
+  let elementoOperacao = document.getElementById("operacao");
+  let elementoResultado = document.getElementById("resultado");
+
+  input01.value = "";
+  input02.value = "";
+  resultado.innerText = "Resultado: ";
 }
