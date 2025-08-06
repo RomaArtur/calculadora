1d9b50d3e0594910a93d0893e5ff1a9d130da811
Artur Roma Furtado <arturromafurtado@gmail.com>
2025-08-05 22:01:37 -0300

    Adição do botão de reiniciar e alteração no nome de algumas variáveis

--- assets/src/script.js
+++ assets/src/script.js
@@ -1,40 +1,50 @@
-console.log('Script carregado!')
+console.log("Script carregado!");
 
 function calcular() {
-    console.log("Função calcular chamada");
-    let input1 = document.getElementById('input01');
-    let input2 = document.getElementById('input02');
-    let valor1 = Number(input1.value);
-    let valor2 = Number(input2.value);
-    console.log(valor1);
-    console.log(valor2);
+  console.log("Função calcular chamada");
+  let elementoInput01 = document.getElementById("input01");
+  let elementoInput02 = document.getElementById("input02");
+  let elementoResultado = document.getElementById("resultado");
+  let valor1 = Number(elementoInput01.value);
+  let valor2 = Number(elementoInput02.value);
+  console.log(valor1);
+  console.log(valor2);
 
-    let operacao = document.getElementById('operacao');
-    let tipoOperacao = operacao.value;
-    console.log(tipoOperacao); 
+  let elementoOperacao = document.getElementById("operacao");
+  let tipoOperacao = operacao.value;
+  console.log(tipoOperacao);
 
-    let resultado;
+  let resultado;
 
-    switch (tipoOperacao) {
-        case "+":
-            resultado = valor1 + valor2;
-            break;
-        case "-":
-            resultado = valor1 - valor2;
-            break;
-        case "*":
-            resultado = valor1 * valor2;
-            break;
-        case "/":
-            resultado = valor1 / valor2;
-            break;
-        default:
-            resultado = alert('Operação inválida');
-    }
-
-    console.log(resultado);
-
+  switch (tipoOperacao) {
+    case "+":
+      resultado = valor1 + valor2;
+      break;
+    case "-":
+      resultado = valor1 - valor2;
+      break;
+    case "*":
+      resultado = valor1 * valor2;
+      break;
+    case "/":
+      if (valor2 !== 0) {
+        resultado = valor1 / valor2;
+      } else {
+        alert("Erro: divisão por zero!");
+        resultado = "";
+      }
+  }
+  elementoResultado.innerText = `Resultado: ${resultado}`;
+  console.log(resultado);
 }
 
-
-
+function limpar() {
+    let elementoInput01 = document.getElementById("input01");
+    let elementoInput02 = document.getElementById("input02");
+    let elementoOperacao = document.getElementById('operacao');
+    let elementoResultado = document.getElementById("resultado");
+    input1.value = '';
+    input2.value = '';
+    operacao.selectedIndex = 0
+    resultado.innerText = 'Resultado: ';
+}
